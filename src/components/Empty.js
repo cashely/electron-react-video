739f453d7c47e10963904bd2264d3926171d6efd
cashely <HONGLOVEMING@129>
2023-02-28 09:01:12 +0800

    add: 新增布局

---
+++ src/components/Empty.js
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+
+function Empty() {
+    return (
+        <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', alignContent: 'center' }}>
+            <Button>
+                新建直播间
+            </Button>
+        </div>
+    )
+}
+
+export default Empty;
